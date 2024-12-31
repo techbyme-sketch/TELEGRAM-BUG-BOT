9e21fc7e7135b148a446df9f596d6be647b36b39
X-TECH CORP <zabsonrejacques30@gmail.com>
2024-12-31 18:58:14 +0100

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "7575081094:AAExUjGgE8zSM4cdgiuVYjw5Sk5KajoZ3f8", // Token bot Telegram
-  OWNER_ID: "6552202106", // ID pemilik bot
+  BOT_TOKEN: "7808685810:AAGVirc-vRyYC-JxsdLvG4yi69U5SbhvuEA", // Token bot Telegram
+  OWNER_ID: "7692631280", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
