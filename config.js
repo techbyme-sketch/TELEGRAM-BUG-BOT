5947d98287a50ca5151c6f40b83187cb5c046ed6
techbyme-sketch <derniercompte874@gmail.com>
2025-05-07 10:20:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "7808685810:AAGVirc-vRyYC-JxsdLvG4yi69U5SbhvuEA", // Token bot Telegram
+  BOT_TOKEN: "7947636670:AAHfdl2FS_OXusQOAHK6M9OaNB08UihL_sI", // Token bot Telegram
   OWNER_ID: "7692631280", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
